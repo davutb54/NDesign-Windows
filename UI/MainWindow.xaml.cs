99c3dcedbf3b6f08ff2b12a5749cbc6253d0caa3
Davut Budak <54davutbudak@gmail.com>
2022-07-06 18:25:17 +0300

    6 July 2022 -Commit 3

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,12 +25,12 @@ namespace UI
         public BitmapImage Image { get; set; } = new BitmapImage(new Uri("C:\\Users\\HİTMACHİNE\\Desktop\\logo.png"));
         public String Username { get; set; } = "Deneme Sürümü";
 
-        private bool _isMaximized;
+        public bool İsMaximized { get; private set; }
 
         public MainWindow()
         {
-            DataContext = this;
             InitializeComponent();
+            DataContext = this;
         }
 
         private void TopPanel_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -43,17 +45,17 @@ namespace UI
 
         private void MaximizeButton_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (_isMaximized)
+            if (İsMaximized)
             {
                 WindowState = WindowState.Normal;
                 MaximizeButton.Source = new BitmapImage(new Uri(@"/Resources/Images/maximize.png",UriKind.Relative));
-                _isMaximized = false;
+                İsMaximized = false;
             }
             else
             {
                 WindowState = WindowState.Maximized;
                 MaximizeButton.Source = new BitmapImage(new Uri(@"/Resources/Images/size.png",UriKind.Relative));
-                _isMaximized = true;
+                İsMaximized = true;
             }
         }
 
