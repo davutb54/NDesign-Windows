78ddfafac8553018279b446170df7c54810d9868
Davut Budak <54davutbudak@gmail.com>
2022-07-08 20:29:12 +0300

    8 July 2022 -Unit Object Created

---
+++ DataAccess/Concrete/EntityFramework/EfUnitDal.cs
@@ -0,0 +1,10 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework;
+
+public class EfUnitDal : EfEntityRepositoryBase<Unit,AppDbContext>
+{
+    
+}
